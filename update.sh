dbba01c3427e3187f64fb123b3336e1716099e93
Edgar Costa <edgarc@mit.edu>
2022-10-04 17:06:03 -0400

    update update script

--- update.sh
+++ update.sh
@@ -1,1 +1,1 @@
-git pull && git submodule sync --recursive && git submodule update --remote --jobs=10
+git pull && git submodule sync --recursive && git submodule update --recursive --remote --jobs=10
