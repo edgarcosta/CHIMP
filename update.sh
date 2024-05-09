55ffda48beb5b9a3e1c77d0e22c0f60fb394bc58
Edgar Costa <edgarc@mit.edu>
2024-05-09 18:16:51 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 set +e
 git pull --recurse-submodules=no # just update CHIMP
-git submodule update --recursive --remote --jobs=10 # updates current repos
+git submodule update --recursive --init --remote --jobs=10 # updates current repos
 set -e
